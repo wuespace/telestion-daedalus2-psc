0fac1f45d50d3838395d446d65297671c07a833a
Jan Tischhoefer <jan.tischhoefer@gmail.com>
2021-02-05 14:20:24 +0100

    Minor config changes + predefined channels

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,12 +1,16 @@
 import { useEffect } from "react";
 import { Widget } from "@wuespace/telestion-client-types";
-import { TelestionClient, Pages, useEventBusManager } from "@wuespace/telestion-client-core";
 import {
-	CommonWrapper,
-	LoginPage,
-	DashboardPage,
-	NotFoundPage,
-	useUserConfig
+  TelestionClient,
+  Pages,
+  useEventBusManager,
+} from "@wuespace/telestion-client-core";
+import {
+  CommonWrapper,
+  LoginPage,
+  DashboardPage,
+  NotFoundPage,
+  useUserConfig,
 } from "@wuespace/telestion-client-common";
 
 import { userConfig } from "../model/sample-user-config";
@@ -16,29 +20,29 @@ import { projectWidgets } from "../widgets";
 const widgets: Array<Widget> = [...projectWidgets];
 
 export function App() {
-	useEventBusManager();
+  useEventBusManager();
 
-	const set = useUserConfig(state => state.set);
+  const set = useUserConfig((state) => state.set);
 
-	useEffect(() => {
-		// apply user config once
-		set(userConfig);
-	}, [set]);
+  useEffect(() => {
+    // apply user config once
+    set(userConfig);
+  }, [set]);
 
-	return (
-		<TelestionClient
-			title="Daedalus 2 Groundstation"
-			wrapper={children => (
-				<CommonWrapper widgets={widgets}>
-					<>{children}</>
-				</CommonWrapper>
-			)}
-		>
-			<Pages preNodes={<Header/>}>
-				<LoginPage/>
-				<DashboardPage/>
-				<NotFoundPage/>
-			</Pages>
-		</TelestionClient>
-	);
+  return (
+    <TelestionClient
+      title="Rocketsound Groundstation"
+      wrapper={(children) => (
+        <CommonWrapper widgets={widgets}>
+          <>{children}</>
+        </CommonWrapper>
+      )}
+    >
+      <Pages preNodes={<Header />}>
+        <LoginPage />
+        <DashboardPage />
+        <NotFoundPage />
+      </Pages>
+    </TelestionClient>
+  );
 }
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,6 +1,5 @@
 import { Widget } from "@wuespace/telestion-client-types";
 import { widget as sampleWidget } from "./sample-widget";
+import { widget as waveformWidget } from "./waveform-widget";
 
-export const projectWidgets: Array<Widget> = [
-	sampleWidget
-];
+export const projectWidgets: Array<Widget> = [sampleWidget, waveformWidget];
